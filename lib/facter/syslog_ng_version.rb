8988bec2edccf3d1a5f059d27593db86451a6d90
Fᴀʙɪᴇɴ Wᴇʀɴʟɪ <wernli@in2p3.fr>
2018-04-26 14:54:27 +0200

    improve error handling of version fact

--- lib/facter/syslog_ng_version.rb
+++ lib/facter/syslog_ng_version.rb
@@ -1,6 +1,10 @@
 Facter.add(:syslog_ng_version) do
   setcode do
-    Facter::Util::Resolution.exec("/usr/sbin/syslog-ng --version").lines.find { |l| l =~ /^syslog-ng/}.split(' ')[1]
+    if Facter::Util::Resolution.which('syslog-ng')
+      Facter::Util::Resolution.exec("syslog-ng --version").lines.find { |l| l =~ /^syslog-ng/}.split(' ')[1]
+    else
+      "3.5"
+    end
   end
 end
 
